bab164b7bd64aade5aab4169d5a3642c84ddce69
ssednei <santana.ednei@gmail.com>
2026-03-22 17:13:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import requests
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # =========================
 # LOAD DATA
 # =========================
